7c42351f8587f8fc18553980f0a58d91a80929e5
sv.pecherkin <sv.pecherkin@tensor.ru>
2021-02-25 18:02:21 +0700

    https://online.sbis.ru/opendoc.html?guid=5b032daf-7b0c-4ae0-b10c-2e9621b33f0a

--- HelloWorld/Start.ts
+++ HelloWorld/Start.ts
@@ -1,13 +1,13 @@
-((resourceRoot) => {
-    function generateElement(key, path) {
+((resourceRoot: string) => {
+    function generateElement(key: string, path: string): Promise<void> {
         return new Promise((resolve) => {
             const element = document.createElement('script');
             element.setAttribute('key', key);
             element.setAttribute('type', 'text/javascript');
             element.setAttribute('src', `/${resourceRoot}/${path}.js`);
-            element.addEventListener('load', resolve);
+            element.addEventListener('load', resolve.bind(null));
             document.head.appendChild(element);
-        })
+        });
     }
 
     const resources = {
@@ -29,12 +29,12 @@
     Promise.all(Object.keys(resources).map((key) => {
         return generateElement(key, resources[key]);
     })).then(() => {
-        require(['Application/Initializer', 'UI/Base'], (AppInit, UI) => {
+        requirejs(['Application/Initializer', 'UI/Base'], (AppInit: any, UI: any) => {
             // @ts-ignore
             window.startContextData = {AppData: new UI.AppData({})};
             // @ts-ignore
-            AppInit.default(window.wsConfig, void 0, new UI.StateReceiver());
+            AppInit.default(window.wsConfig);
             UI.Start({}, document.getElementById('wasabyRoot'));
         });
-    })
+    });
 })('wasaby');
